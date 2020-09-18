b74c1076fa4e6ea9211e3459406eeb356e566cbd
Dream-By <dzmitry.dzianisau@gmail.com>
2020-09-18 10:53:46 +0300

    add new features

--- app/src/main/java/com/example/appwidgetmanager/CatDaysAppWidget.java
+++ app/src/main/java/com/example/appwidgetmanager/CatDaysAppWidget.java
@@ -5,6 +5,9 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -25,8 +28,12 @@ public class CatDaysAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        //for (int appWidgetId : appWidgetIds) {
+        //    updateAppWidget(context, appWidgetManager, appWidgetId);
+        //}
+        final int N = appWidgetIds.length;
+        for (int i = 0; i<N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
         }
     }
 
@@ -39,5 +46,23 @@ public class CatDaysAppWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,int[] appWidgetId) {
+
+        Calendar catDay = new GregorianCalendar(2016, 2, 1);
+        Calendar today = Calendar.getInstance();
+        long days = ((catDay.getTimeInMillis() - today.getTimeInMillis()) / (24 * 60 * 60 * 1000)) + 1;
+
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.cat_days_app_widget);
+        // views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        views.setTextViewText(R.id.appwidget_text, "Осталось дней: " + days);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
 }
 
